1ae1cef9fc0384c4aea20d7daed13312d1617c86
Asieertxo <asgal.gl@gmail.com>
2022-01-17 20:47:12 +0100

    nav ok, titulo y fondo

---
+++ src/components/Title/Title.js
@@ -0,0 +1,14 @@
+import "./Title.scss";
+
+function Title() {
+  return (
+    <div className="mainheader-titlebox">
+      {/*<h1 className="mainheader-title">*/}
+        <span className="mainheader-titlebox__main">Hotel TheLan</span>
+        <span className="mainheader-titlebox__secondary">demuestra lo que sabes hacer</span>
+      {/*</h1>*/}
+    </div>
+  );
+}
+
+export default Title;
